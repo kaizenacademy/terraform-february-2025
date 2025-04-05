10c856b7a3b3986baafcbc46f9dea526b4b24731
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-05 17:42:09 +0000

    new change

--- Class3-part2/key.tf
+++ Class3-part2/key.tf
@@ -1,4 +1,17 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = file("~/.ssh/id_rsa.pub")
+
+  tags = {
+    Name = "Hello"
+    key = "value"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-adileta"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
