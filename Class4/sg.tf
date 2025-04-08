178521f4b9ca582af3038e8b7097751d0e954183
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-08 23:37:24 +0000

    new change

--- Class4/sg.tf
+++ Class4/sg.tf
@@ -1,20 +1,19 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.main.id
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
+    from_port        = var.port1
+    to_port          = var.port1
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
     ingress {
     description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
+    from_port        = var.port2
+    to_port          = var.port2
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
--- Class4/variables.tf
+++ Class4/variables.tf
@@ -5,3 +5,11 @@ variable aws_key {
 variable region {
   default = "us-east-2"
 }
+
+variable port1 {
+    default = 22
+}
+
+variable port2 {
+    default = 80
+}
