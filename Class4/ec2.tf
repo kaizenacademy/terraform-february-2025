af2a6bd577d9597a8dc3db853c6b6f33897d5331
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-09 02:01:33 +0000

    new change

--- Class4/ec2.tf
+++ Class4/ec2.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "web" {
-  ami           = var.ec2.ami
-  instance_type = var.ec2.type
+  ami           = var.ec2[0].ami
+  instance_type = var.ec2[0].type
+}
 
-  tags = var.ec2.tags
+resource "aws_instance" "web2" {
+  ami           = var.ec2[1].ami
+  instance_type = var.ec2[1].type
 }
--- Class4/variables.tf
+++ Class4/variables.tf
@@ -13,21 +13,38 @@ variable "port" {
   type    = list(number)
 }
 
+# variable ec2 {
+#   type = object({
+#     ami = string
+#     type = string
+#     tags = map(string)
+#   })
+  
+#   default = {
+#     ami = "ami-0100e595e1cc1ff7f"
+#     type = "t2.micro"
+#     tags = {
+#     Environment = "dev"
+#     Team        = "DevOps"
+#     Project     = "Kaizen"
+#   }
+#   }
+# }
 
 variable ec2 {
-  type = object({
+  type = list(object({
     ami = string
     type = string
-    tags = map(string)
-  })
+  }))
   
-  default = {
+  default = [
+    {
     ami = "ami-0100e595e1cc1ff7f"
     type = "t2.micro"
-    tags = {
-    Environment = "dev"
-    Team        = "DevOps"
-    Project     = "Kaizen"
+  },
+  {
+    ami = "ami-0c3b809fcf2445b6a"
+    type = "t2.micro"
   }
-  }
+  ]
 }
