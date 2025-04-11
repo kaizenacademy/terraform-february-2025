61ae46d8d450cca1f073cd08d476ff738f37392a
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-11 01:20:31 +0000

    new change

--- Class5-part2/main.tf
+++ Class5-part2/main.tf
@@ -18,6 +18,10 @@ resource "aws_instance" "web" {
 
 
   provisioner "remote-exec" {
-    inline = ["mkdir kaizen"]
+    inline = [
+        "sudo yum install httpd -y",
+        "sudo systemctl start httpd",
+        "sudo systemctl status httpd"
+        ]
   }
 }
