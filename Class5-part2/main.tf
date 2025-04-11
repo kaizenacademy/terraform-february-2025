75f321fc5caafb7e5f308c6caf3e3e067ada83fb
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-11 01:22:26 +0000

    new change

--- Class5-part2/main.tf
+++ Class5-part2/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "web" {
     inline = [
         "sudo yum install httpd -y",
         "sudo systemctl start httpd",
-        "sudo systemctl status httpd"
+        "sudo systemctl enable httpd"
         ]
   }
 }
