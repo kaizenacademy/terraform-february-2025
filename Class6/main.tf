cee5848459cc94a79f1177cdf88fc86a4f9730c1
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-12 16:39:52 +0000

    new files

---
+++ Class6/main.tf
@@ -0,0 +1,9 @@
+module "vpc1" {
+  source  = "kaizenacademy/vpc1/aws"
+  version = "0.2.0"
+
+  vpc_cidr = "172.31.0.0/16"
+  subnet1_cidr = "172.31.1.0/24"
+  subnet2_cidr = "172.31.2.0/24"
+  environment = "qa"
+}
