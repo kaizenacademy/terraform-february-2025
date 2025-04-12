f11266450c07c44fada503f91860cfad211396ca
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-12 18:18:56 +0000

    module files

---
+++ Modules/github/main.tf
@@ -0,0 +1,13 @@
+provider aws {
+    region = "us-east-1"
+}
+
+module "vpc1" {
+  source  = "git@github.com:kaizenacademy/terraform-aws-vpc1.git?ref=v0.1.0"
+
+  vpc_cidr = "172.31.0.0/16"
+  subnet1_cidr = "172.31.1.0/24"
+  #subnet2_cidr = "172.31.2.0/24"
+  environment = "stage"
+}
+
